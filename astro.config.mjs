a0ef3e90bf0ca3b19d57557cdeee6c4f4ec1bba8
Miguel do Rosario <migueldorosario2@gmail.com>
2026-07-13 17:41:00 -0300

    chore: redirect old typo url (barria) to corrected url (barreira)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mundotrilhos.com',
+	redirects: {
+		'/blog/20260712-china-rompe-a-barria-dos-450-kmh-e-reescreve-a-geografia-do-desenvolvimento-sobr/': '/blog/20260712-china-rompe-a-barreira-dos-450-kmh-e-reescreve-a-geografia-do-desenvolvimento-sobr/',
+		'/blog/20260712-china-rompe-a-barria-dos-450-kmh-e-reescreve-a-geografia-do-desenvolvimento-sobr': '/blog/20260712-china-rompe-a-barreira-dos-450-kmh-e-reescreve-a-geografia-do-desenvolvimento-sobr',
+	},
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
